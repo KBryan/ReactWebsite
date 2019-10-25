8f3f478e70e69c65ae71b2b1364207cf2c264145
Kwame Bryan <kwame.bryan@gmail.com>
2019-10-25 16:41:14 -0400

    Added Routes to and bootstrap to project. Now working with dynamic data

--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -1,60 +1,74 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './TopNav.css';
 import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem } from 'reactstrap';
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem } from 'reactstrap';
 
-const TopNav = (props) => {
-  const [isOpen, setIsOpen] = useState(false);
+import {Link} from 'react-router-dom';
 
-  const toggle = () => setIsOpen(!isOpen);
 
-  return (
-    <div>
-      <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">reactstrap</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="ml-auto" navbar>
-            <NavItem>
-              <NavLink href="/components/">Components</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-            </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Options
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>
-                  Option 1
-                </DropdownItem>
-                <DropdownItem>
-                  Option 2
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>
-                  Reset
-                </DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </Nav>
-        </Collapse>
-      </Navbar>
-    </div>
-  );
+class TopNav extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+         isOpen: false
+        };
+    }
+
+    toggle() {
+        this.setState({
+          isOpen: !this.state.isOpen
+        });
+    }
+
+    render(){
+        const VehicleSelections = this.props.vehicleData.map((item) => {
+          return <DropdownItem key={item.detailKey}>
+            <Link to={{pathname: "/detail/" + item.detailKey}}>
+              {item.model}
+            </Link>
+          </DropdownItem>
+        }, this);
+
+        return(<div>
+            <Navbar color="light" light expand="md">
+              <NavbarBrand href="/">flying car dealership</NavbarBrand>
+              <NavbarToggler onClick={this.toggle} />
+              <Collapse isOpen={this.state.isOpen} navbar>
+                <Nav className="ml-auto" navbar>
+                  <NavItem>
+                    <NavLink href="/"><i className="fas fa-home"></i> Home</NavLink>
+                  </NavItem>
+                  <UncontrolledDropdown nav inNavbar>
+                    <DropdownToggle nav caret>
+                    <i className="fas fa-space-shuttle"></i> Flying Cars
+                    </DropdownToggle>
+                    <DropdownMenu right>
+                      {VehicleSelections}
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                  <NavItem>
+                      <NavLink href='/find-a-dealer'><i className="fas fa-map-marker-alt"></i> Find A Dealer</NavLink>
+                  </NavItem>
+                  <NavItem>
+                      <NavLink href='/build-and-price'><i className="fas fa-cog"></i> Build And Price</NavLink>
+                  </NavItem>
+                </Nav>
+              </Collapse>
+            </Navbar>
+          </div>);
+    }
 }
 
 export default TopNav;
-
-
